bfd96c5872a5e86e94eb91cca6e789669876f3c6
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-09-24 21:52:09 +0200

    Replace quotes with double quotes in foreign script.

--- src/Bookmarks/Foreign.js
+++ src/Bookmarks/Foreign.js
@@ -44,15 +44,15 @@ function call2(method, err) {
   };
 }
 
-exports.getImpl         = call1('get', 'Could not get bookmark');
-exports.getManyImpl     = call1('get', 'Could not get bookmarks');
-exports.getChildrenImpl = call1('getChildren', 'Could not get children');
-exports.getRecentImpl   = call1('getRecent', 'Could not get recent');
-exports.getTreeImpl     = call0('getTree', 'Could not get tree');
-exports.getSubTreeImpl  = call1('getSubTree', 'Could not get sub-tree');
-exports.searchImpl      = call1('search', 'Could not perform search');
-exports.createImpl      = call1('create', 'Could not create bookmark');
-exports.moveImpl        = call2('move', 'Could not move bookmark');
-exports.updateImpl      = call2('update', 'Could not update bookmark');
-exports.removeImpl      = call1('remove', 'Could not remove bookmark');
-exports.removeTreeImpl  = call1('removeTree', 'Could not remove tree');
+exports.getImpl         = call1("get", "Could not get bookmark");
+exports.getManyImpl     = call1("get", "Could not get bookmarks");
+exports.getChildrenImpl = call1("getChildren", "Could not get children");
+exports.getRecentImpl   = call1("getRecent", "Could not get recent");
+exports.getTreeImpl     = call0("getTree", "Could not get tree");
+exports.getSubTreeImpl  = call1("getSubTree", "Could not get sub-tree");
+exports.searchImpl      = call1("search", "Could not perform search");
+exports.createImpl      = call1("create", "Could not create bookmark");
+exports.moveImpl        = call2("move", "Could not move bookmark");
+exports.updateImpl      = call2("update", "Could not update bookmark");
+exports.removeImpl      = call1("remove", "Could not remove bookmark");
+exports.removeTreeImpl  = call1("removeTree", "Could not remove tree");
